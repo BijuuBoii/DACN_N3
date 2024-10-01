0dafaf72493e087d20b565a9cd79015427bd1ca4
datmoitapcode <144819139+datmoitapcode@users.noreply.github.com>
2024-10-01 23:33:30 +0700

    update banner va carousel va form login

---
+++ DACN_N3/Controllers/AuthorityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DACN_N3.Controllers
+{
+    public class AuthorityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
