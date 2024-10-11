4ce155fcc250818dcd172d578546eb54672b89a5
datmoitapcode <144819139+datmoitapcode@users.noreply.github.com>
2024-10-11 21:52:16 +0700

    admin

---
+++ DACN_N3/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DACN_N3.Controllers
+{
+	public class AdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Admin()
+		{
+			return View();
+		}
+	}
+}
