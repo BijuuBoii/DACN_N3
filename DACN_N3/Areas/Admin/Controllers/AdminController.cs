8059366d0963b72520a9afa9c17c8e0bccbea93b
datmoitapcode <144819139+datmoitapcode@users.noreply.github.com>
2024-10-16 21:35:18 +0700

    di chuyển admin qua areas

---
+++ DACN_N3/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DACN_N3.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
