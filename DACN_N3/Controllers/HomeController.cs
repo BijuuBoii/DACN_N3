5adee9a22bc0a3c16ee76506113f7835f10f9624
boi <boikhoibo@gmail.com>
2024-11-02 23:52:08 +0700

    fix xong chuyển phần

--- DACN_N3/Controllers/HomeController.cs
+++ DACN_N3/Controllers/HomeController.cs
@@ -32,13 +32,26 @@ namespace DACN_N3.Controllers
 		    .Include(m => m.Seasons) // Bao gồm các mùa
 			.ThenInclude(s => s.Episodes) // Bao gồm các tập
 		    .FirstOrDefault(m => m.MovieId == id);
-
+            ViewBag.SeasonNumber = 1;
 			ViewBag.Movie = movie;
 			ViewBag.Seasons = movie.Seasons.ToList();
 			ViewBag.Episodes = movie.Seasons.SelectMany(s => s.Episodes).ToList();
 			return View();
         }
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		[HttpPost]
+		public IActionResult UpdateFilmDetails(int id, int seasonNumber)
+		{
+			var movie = _movieDbContext.Movies
+			.Include(m => m.Seasons) // Bao gồm các mùa
+			.ThenInclude(s => s.Episodes) // Bao gồm các tập
+			.FirstOrDefault(m => m.MovieId == id);
+			ViewBag.SeasonNumber = seasonNumber;
+			ViewBag.Movie = movie;
+			ViewBag.Seasons = movie.Seasons.ToList();
+			ViewBag.Episodes = movie.Seasons.SelectMany(s => s.Episodes).ToList();
+			return View("FilmDetails"); // Trả về view với dữ liệu mới
+		}
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
