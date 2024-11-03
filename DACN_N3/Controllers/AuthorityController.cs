9cde159e2932d6f8fbbb479e7fa31c6287430b51
boi <boikhoibo@gmail.com>
2024-11-03 15:02:44 +0700

    nav item thể loại

--- DACN_N3/Controllers/AuthorityController.cs
+++ DACN_N3/Controllers/AuthorityController.cs
@@ -1,17 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DACN_N3.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Scripting;
 
 namespace DACN_N3.Controllers
 {
     public class AuthorityController : Controller
     {
-        public IActionResult Index()
+		private MovieDbContext _movieDbContext;
+		public AuthorityController(MovieDbContext movieDbContext)
+		{
+			_movieDbContext = movieDbContext;
+		}
+		public IActionResult Index()
         {
             return View();
         }
+		public IActionResult Login()
+		{
+			var genres = _movieDbContext.Genres.ToList(); // Lấy danh sách thể loại
+			ViewBag.AllGenres = genres; // Gửi danh sách thể loại vào ViewBag
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Login(User user)
+		{
+			var genres = _movieDbContext.Genres.ToList(); // Lấy danh sách thể loại
+			ViewBag.AllGenres = genres; // Gửi danh sách thể loại vào ViewBag
 
-        public IActionResult Login()
-        {
-            return View();
-        }
-    }
+			var existingUser = _movieDbContext.Users.SingleOrDefault(u => u.Email == user.Email);
+			if (existingUser != null && user.Password == existingUser.Password)
+			{
+				// Đăng nhập thành công
+				return RedirectToAction("Index", "Home");
+			}
+			// Xử lý đăng nhập không thành công
+			ModelState.AddModelError(string.Empty, "Đăng nhập không hợp lệ.");
+			return View(user);
+		}
+		[HttpPost]
+		public IActionResult SignUp(User user)
+		{
+			if (user.Password != null && user.Email != null && user.Username != null)
+			{
+				user.Password = user.Password;
+				user.CreatedDate = DateTime.Now;
+				user.Role = "User";
+				_movieDbContext.Users.Add(user);
+				_movieDbContext.SaveChanges();
+				
+			}
+			ModelState.AddModelError(string.Empty, "Đăng ký không hợp lệ.");
+			return RedirectToAction("Login");
+
+		}
+
+
+
+	}
 }
--- DACN_N3/Controllers/HomeController.cs
+++ DACN_N3/Controllers/HomeController.cs
@@ -10,15 +10,30 @@ namespace DACN_N3.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private MovieDbContext _movieDbContext;
+        private int UserID;
         public HomeController(ILogger<HomeController> logger, MovieDbContext movieDbContext)
         {
             _logger = logger;
             _movieDbContext = movieDbContext;
+            UserID = 0;
         }
        
         public IActionResult Index()
         {
-            var movies  = _movieDbContext.Movies.ToList();
+            var movies  = _movieDbContext.Movies.Include(m => m.Genres).ToList();
+            
+            var animeMovies = movies.Where(m => m.Genres.Any(g => g.Name == "Anime")).ToList();
+            var crimeMovies = movies.Where(m => m.Genres.Any(g => g.Name == "Crime")).ToList();
+            
+            var randomMovies = movies.OrderBy(m => Guid.NewGuid()) // Sắp xếp ngẫu nhiên
+		    .FirstOrDefault(); // Lấy phim đầu tiên
+
+			var genres = _movieDbContext.Genres.ToList(); // Lấy danh sách thể loại
+			ViewBag.AllGenres = genres; // Gửi danh sách thể loại vào ViewBag
+
+			ViewBag.AnimeMovies = animeMovies;
+            ViewBag.CrimeMovies = crimeMovies;
+            ViewBag.RandomMovies = randomMovies;
             return View(movies);
         }
         public IActionResult Privacy()
@@ -31,11 +46,17 @@ namespace DACN_N3.Controllers
 			var movie = _movieDbContext.Movies
 		    .Include(m => m.Seasons) // Bao gồm các mùa
 			.ThenInclude(s => s.Episodes) // Bao gồm các tập
-		    .FirstOrDefault(m => m.MovieId == id);
-            ViewBag.SeasonNumber = 1;
+			.Include(g => g.Genres)
+			.FirstOrDefault(m => m.MovieId == id);
+
+			var genres = _movieDbContext.Genres.ToList(); // Lấy danh sách thể loại
+			ViewBag.AllGenres = genres; // Gửi danh sách thể loại vào ViewBag
+
+			ViewBag.SeasonNumber = 1;
 			ViewBag.Movie = movie;
 			ViewBag.Seasons = movie.Seasons.ToList();
 			ViewBag.Episodes = movie.Seasons.SelectMany(s => s.Episodes).ToList();
+            ViewBag.Genres = movie.Genres.ToList();
 			return View();
         }
 		[HttpPost]
@@ -43,14 +64,36 @@ namespace DACN_N3.Controllers
 		{
 			var movie = _movieDbContext.Movies
 			.Include(m => m.Seasons) // Bao gồm các mùa
-			.ThenInclude(s => s.Episodes) // Bao gồm các tập
+			.ThenInclude(s => s.Episodes)// Bao gồm các tập
+			.Include(g => g.Genres)
 			.FirstOrDefault(m => m.MovieId == id);
+
+			var genres = _movieDbContext.Genres.ToList(); // Lấy danh sách thể loại
+			ViewBag.AllGenres = genres; // Gửi danh sách thể loại vào ViewBag
+
 			ViewBag.SeasonNumber = seasonNumber;
 			ViewBag.Movie = movie;
 			ViewBag.Seasons = movie.Seasons.ToList();
 			ViewBag.Episodes = movie.Seasons.SelectMany(s => s.Episodes).ToList();
+			ViewBag.Genres = movie.Genres.ToList();
 			return View("FilmDetails"); // Trả về view với dữ liệu mới
 		}
+
+        public IActionResult ListFilm(int id)
+        {
+            var genres = _movieDbContext.Genres.ToList(); // Lấy danh sách thể loại
+            ViewBag.AllGenres = genres; // Gửi danh sách thể loại vào ViewBag
+            
+            var movies = _movieDbContext.Movies.Include(g => g.Genres).ToList();
+            var relatedFilms = movies.Where(m => m.Genres.Any(g => g.GenreId == id)).ToList();
+
+            var genreName = _movieDbContext.Genres.FirstOrDefault(g => g.GenreId == id)?.Name;
+            ViewBag.GenreName = genreName;
+
+            ViewBag.RelatedFilms = relatedFilms;
+            return View();
+        }
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
